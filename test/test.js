58deab504db6656f5fff34260dfb5d400fb62bd2
Billy Schulze <billyschulze97@gmail.com>
2018-08-12 17:35:38 +1000

    add chai and nyc for testing

--- test/test.js
+++ test/test.js
@@ -1,13 +1,22 @@
-const assert = require("assert");
+const assert = require("chai").assert;
 const Player = require("../game/player");
 
-describe("Player", () => {
+describe("Player", function() {
+
     const deck = [];
     const player = new Player(deck);
-    describe("#createCounter()", () => {
-        player.createCounter("test", 1);
-        it("should have a counter \"test\" with the value of 1", () => {
+
+    describe("#createCounter()", function() {
+        it("should have a counter \"test\" with the value of 1", function() {
+            player.createCounter("test", 1);
             assert.equal(player.counters.test, 1);
+        });
+    });
+
+    describe("#updateCounter()", function() {
+        it("should have a counter \"test\" with the value of 4, (1 + 3)", function() {
+            player.updateCounter("test", 3);
+            assert.equal(player.counters.test, 4);
         });
     });
 });
